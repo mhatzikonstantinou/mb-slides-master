2e4221fbb0280bc23cf74a4537012d9b6818563f
Manolis Chatzikonstantinou <ec1269@georgetown.edu>
2022-08-21 13:16:51 +0300

    'Week3to5'

---
+++ render.R
@@ -0,0 +1,5 @@
+# Rendering all the slides
+
+renderthis::to_pdf(from = "presentation3.Rmd","Econ_465_Money_and_Banking_p5.pdf",partial_slides = T,complex_slides = T)
+renderthis::to_pdf(from = "presentation4.Rmd","Econ_465_Money_and_Banking_p6.pdf",partial_slides = T,complex_slides = T)
+renderthis::to_pdf(from = "presentation5.Rmd","Econ_465_Money_and_Banking_p7.pdf",partial_slides = T,complex_slides = T)
